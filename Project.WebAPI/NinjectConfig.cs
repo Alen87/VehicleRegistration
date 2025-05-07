0e8448b505450ef95ba8d5f148b5b9155f05b8d4
Alen Boric <boricalen@live.com>
2025-05-07 22:36:02 +0200

    feat>ninjectConfig

--- Project.WebAPI/NinjectConfig.cs
+++ Project.WebAPI/NinjectConfig.cs
@@ -4,6 +4,8 @@ using Project.Repository.Common;
 using Project.Service;
 using Project.Service.Common;
 using AutoMapper;
+using Project.Model;
+using Project.DAL.Entities;
 
 namespace Project.WebAPI
 {
@@ -29,7 +31,22 @@ namespace Project.WebAPI
            
             var mapperConfiguration = new MapperConfiguration(cfg =>
             {
-               
+                // Entity → Model mapiranja
+                cfg.CreateMap<DAL.Entities.VehicleMake, Model.VehicleMake>();
+                cfg.CreateMap<DAL.Entities.VehicleModel, Model.VehicleModel>()
+                   .ForMember(dest => dest.MakeName, opt => opt.MapFrom(src => src.Make.Name));
+                cfg.CreateMap<DAL.Entities.VehicleOwner, Model.VehicleOwner>();
+                cfg.CreateMap<DAL.Entities.VehicleEngineType, Model.VehicleEngineType>();
+                cfg.CreateMap<DAL.Entities.VehicleRegistration, Model.VehicleRegistration>()
+                   .ForMember(dest => dest.ModelName, opt => opt.MapFrom(src => src.Model.Name))
+                   .ForMember(dest => dest.OwnerName, opt => opt.MapFrom(src => src.Owner.FirstName + " " + src.Owner.LastName));
+                
+                // Model → Entity mapiranja
+                cfg.CreateMap<Model.VehicleMake, DAL.Entities.VehicleMake>();
+                cfg.CreateMap<Model.VehicleModel, DAL.Entities.VehicleModel>();
+                cfg.CreateMap<Model.VehicleOwner, DAL.Entities.VehicleOwner>();
+                cfg.CreateMap<Model.VehicleEngineType, DAL.Entities.VehicleEngineType>();
+                cfg.CreateMap<Model.VehicleRegistration, DAL.Entities.VehicleRegistration>();
             });
 
             kernel.Bind<IMapper>().ToConstant(mapperConfiguration.CreateMapper()).InSingletonScope();
